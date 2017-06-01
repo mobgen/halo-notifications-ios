902b3ee63099955508f9542a218465aae4cb0248
Borja Santos-Díez <borja.santos@mobgen.com>
2017-06-01 13:02:31 +0200

    Added a way to handle rich notifications through the addon

--- Source/NotificationsAddon+ServiceExtension.swift
+++ Source/NotificationsAddon+ServiceExtension.swift
@@ -1,6 +1,6 @@
 //
-//  NotificationService.swift
-//  HaloRichNotifications
+//  NotificationsAddon+ServiceExtension.swift
+//  HaloNotifications
 //
 //  Created by Santos-Díez, Borja on 01/06/2017.
 //  Copyright © 2017 Mobgen Technology. All rights reserved.
@@ -8,16 +8,12 @@
 
 import UserNotifications
 
-class NotificationService: UNNotificationServiceExtension {
-
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+@available(iOS 10.0, *)
+extension NotificationsAddon {
+    
+    public func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         
-        if let bestAttemptContent = bestAttemptContent,
+        if let bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent,
             let data = request.content.userInfo["data"] as? [String: Any],
             let image = data["image"] as? [String: String],
             let imageUrl = image["url"],
@@ -32,12 +28,10 @@ class NotificationService: UNNotificationServiceExtension {
         }
     }
     
-    override func serviceExtensionTimeWillExpire() {
+    public func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
+        
     }
-
+    
 }
